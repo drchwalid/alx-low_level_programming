40c78cf13cd791e03273d663835c674d592f08f2
drchwalid <drch.walid2005@gmail.com>
2023-09-12 14:38:55 -0700

    pls

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,21 @@
  *        -1 and prints - if n is less than zero.
  */
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
 		_putchar(45);
-		return (0);
+		return (-1);
 	}
 	else
 	{
-		_putchar(48);
-		return (-1);
+		_putchard(48);
+		return (0);
 	}
-}
+}			
